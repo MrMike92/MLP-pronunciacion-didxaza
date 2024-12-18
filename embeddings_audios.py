86643853dba68e227ee4483e196bfc10890a453e
MrMike92 <sandovalpinedamiguelangel@gmail.com>
2024-12-18 13:47:41 -0600

    Corrección de versión del generador de embeddings

--- embeddings_audios.py
+++ embeddings_audios.py
@@ -3,6 +3,7 @@ import torch
 import torchaudio
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
 from transformers import WavLMModel, Wav2Vec2FeatureExtractor
 
 input_folders = {"data/bien": 0, "data/mal": 1}
@@ -29,32 +30,27 @@ for folder, label in input_folders.items():
             if sample_rate != target_sample_rate:
                 resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=target_sample_rate)
                 waveform = resampler(waveform)
-                sample_rate = target_sample_rate
 
             audio = waveform.squeeze().numpy()
-            audio = audio / np.max(np.abs(audio))
-            inputs = feature_extractor(audio, sampling_rate=sample_rate, return_tensors="pt", padding=True)
+            audio = audio / np.max(np.abs(audio)) # Normalizar el audio
+            inputs = feature_extractor(audio, sampling_rate=target_sample_rate, return_tensors="pt", padding=True) # Extraer embeddings
             inputs = inputs.input_values.to(device)
 
             with torch.no_grad():
                 outputs = model(inputs)
                 embeddings = outputs.last_hidden_state.mean(dim=1).squeeze().cpu().numpy()
 
-            if len(embeddings) > embedding_size:
-                embeddings = embeddings[:embedding_size]
-
-            else:
-                embeddings = np.pad(embeddings, (0, embedding_size - len(embeddings)), mode='constant')
-
             data.append(embeddings)
             labels.append(label)
 
 data = np.array(data)
+pca = PCA(n_components=embedding_size)
+data_reduced = pca.fit_transform(data)
 labels = np.array(labels).reshape(-1, 1)
-combined = np.hstack((labels, data))
+combined = np.hstack((labels, data_reduced))
 columns = ["label"] + [f"embedding_{i}" for i in range(embedding_size)]
 df = pd.DataFrame(combined, columns=columns)
 df.to_csv(output_csv, index=False)
-print(f"Archivo CSV generado: {output_csv}")
+print(f"Archivo CSV con PCA generado: {output_csv}")
 
 # Copyright (c) 2024 MrMike92
